fbeddeae269996adc71f8a44ab940d7cb4530d2b
Roger <wb6794519@126.com>
2016-12-12 20:41:14 +0800

    commit all

--- winer_loser_tree.hpp
+++ winer_loser_tree.hpp
@@ -8,10 +8,10 @@ class CTree
 public:
   // 败者树的重构跟胜者树是不一样的，败者树的重构只需要与其父结点比较。
   // Loser tree for 多路平衡归并外部排序
-  void loser_tree();
-  void winner_tree();
+  void create_loser_tree(int* A, int len);
+  void create_winner_tree(int* A, int len);
 
-
+  void kways_merge(int* A, int len);
 private:
 
 
